b78d109b7b054e0c4ae0787d0714935298f05fdb
Jorge Gabitto <jgabitto1792@gmail.com>
2020-11-03 18:31:23 -0600

    Finished

--- web-guesser.rb
+++ web-guesser.rb
@@ -6,8 +6,6 @@ def number_generator
     rand(1..100)
 end
 
-NUMBER = number_generator
-
 GUESS_MESSAGES = {
     1 => "This is your 1st guess",
     2 =>  "This is your 2nd guess",
@@ -15,7 +13,8 @@ GUESS_MESSAGES = {
     4 =>  "This is your 4th guess",
     5 =>  "This is your 5th guess",
     6 =>  "This is your 6th guess",
-    7 =>  "This is your last guess"    
+    7 =>  "This is your last guess",
+    8 => "Game Over"    
 }
 
 HINT_MESSAGES = {
@@ -23,26 +22,54 @@ HINT_MESSAGES = {
     :low =>  "Close, but low",
     :correct =>  "You guessed correctly!",
     :high =>  "Close, but high",
-    :too_high =>  "Too high"  
+    :too_high =>  "Too high",
+    :game_over => "Sorry you suck!"  
 }
 
 get "/" do
+    number = number_generator
+    session[:number] = number
+
     session[:guess_number] = 1
+    
+    session[:hint_message] = nil
+    
     erb :index
 end
 
 get "/start" do
-    @guess_message = GUESS_MESSAGES[guess_number]
+    @number = session[:number]
+    @guess_number = session[:guess_number]
+    @guess_message = GUESS_MESSAGES[@guess_number]
+    @hint_message = session[:hint_message]
+    p @guess_message
+    
     erb :start
 end
 
 post "/start" do
-    guess = params["guess"]
+    guess = Integer(params["guess"])
+    number = session[:number]
+    @guess_number = session[:guess_number]
 
-    if guess == NUMBER
-        @hint_message = HINT_MESSAGES[:correct]
-    elsif guess.between?(NUMBER - 10, Number + 10)
-        @hint_message = HINT_MESSAGES[:correct]
-    session[:guess_number] = guess_number + 1
+    if guess == number
+        session[:hint_message] = HINT_MESSAGES[:correct]
+    elsif guess.between?(number - 10, number)
+        session[:hint_message] = HINT_MESSAGES[:low]
+    elsif guess.between?(number, number + 10)
+        session[:hint_message] = HINT_MESSAGES[:high]
+    elsif guess < number && !guess.between?(number - 10, number)
+        session[:hint_message] = HINT_MESSAGES[:too_low]
+    else
+        session[:hint_message] = HINT_MESSAGES[:too_high]
+    end
+
+    if @guess_number != 7
+        session[:guess_number] = @guess_number + 1
+    else
+        session[:guess_number] = 8
+        session[:hint_message] = HINT_MESSAGES[:game_over]
+    end
+    
     redirect "/start"
 end
